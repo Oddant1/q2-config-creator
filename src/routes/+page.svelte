965ad5be579d73d1a4b6e2ac0cce7702c73da8d5
Oddant1 <oddant1@hotmail.com>
2026-03-13 12:15:21 -0700

    More

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -94,11 +94,11 @@
 	<NavBanner />
 </header>
 <div class="positioned-container min-h-screen overflow-auto bg-white text-black py-10">
-	<div class="max-width flex items-center gap-3 ">
+	<div class="max-width flex items-center">
 		<span class="mx-auto text-4xl font-bold text-black">QIIME 2 Parallel Config Creator</span>
 	</div>
-	<main class="max-width pt-10 pb-12">
-		<p class="pb-4 pt-4">
+	<main class="max-width pt-5 pb-5">
+		<p class="pb-4">
 			QIIME 2 features the ability to parallelize all QIIME 2 Pipelines using the PARSL parallel
 			scripting library under the hood. Running small workflows in parallel on a laptop requires
 			minimal configuration, and can be achieved by simply passing the --p-parallel flag to the
